989841ffaecddb3671cfe424b851e2e02d0e5dfc
Carl Robert Sookael <carl@raulwalter.com>
2023-12-19 00:13:29 +0200

    Initial commit with project structure from spring initializr

---
+++ src/main/java/dev/carlrs/sectorapi/SectorApiApplication.java
@@ -0,0 +1,13 @@
+package dev.carlrs.sectorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carlrs/sectorapi/SectorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlrs.sectorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
